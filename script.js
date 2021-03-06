db6ee6f414dcf1a61951d517f28a84c09686ba9d
Yeoh Chong <yeohcy911@gmail.com>
2021-03-06 02:34:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    document.getElementById('user_div').style.display = 'block'
+    document.getElementById('login_div').style.display = 'none'
+  } else {
+
+    document.getElementById('user_div').style.display = 'none'
+    document.getElementById('login_div').style.display = 'block'
+  }
+})
+
+function login() {
+  var userEmail = document.getElementById('email_field').value
+  var userPass = document.getElementById('password_field').value
+
+  firebase
+    .auth()
+      .signInWithEmailAndPassword(userEmail, userPass)
+      .catch(function (error) {
+        var errorCode = error.code
+        var errorMessage = error.message
+        window.alert('Error : ' + errorMessage)
+    })
+}
+
+function logout() {
+  firebase.auth().signOut()
+}
